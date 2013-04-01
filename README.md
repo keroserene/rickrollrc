1ea67d9a0700ffd98826d7b91e5972f6e5181f32
Serene Han <keroserene@gmail.com>
2013-04-01 09:37:57 -0400

    tests, cleanups

--- README.md
+++ README.md
@@ -4,50 +4,22 @@ Bash script which Rick Rolls your terminal, with the option to enable
 indefinite encores for those who desire intense 80's music video
 appreciation sessions.
 
-![Rick Roll in xterms]()
-
 ## How to Roll
-
 To start rick rollin' immediately:
 
-    curl https://raw.github.com/keroserene/rickrollrc/master/astley.sh | bash
+    curl https://raw.github.com/keroserene/rickrollrc/master/roll.sh | bash
 
-
-## Deets
+![Rick Roll in xterms]()
 
 For most effectiveness, make sure you're on a 256-color terminal.
 (After all, this is a colorful hobby.)
 For the basic experience:
 
-    ./astley.sh
+    ./roll.sh
 
 During Rick's performance, if you wish to prevent rude interruptions and 
 signals from things like Ctrl-C, etc. (and replace them with lyrics).
 
-    ./astley.sh evil
+    ./roll.sh evil
 
-If you *really* will Never Give You Up, 
-then you should append to $USER's bashrc. (highly recommended)
-
-    ./astley.sh inject
-
-To go for the gold:
-
-    ./astley.sh inject evil
-
-Finally, to spread the love,
-chmod 777 on lulz and display prominently in $HOME.
-
-    ./lulz
-
-Which does *everything*. :)
-
-## Misc.
-
-If you've astley-overdosed:
-
-    ./astley.sh stop
-
-Enjoy! Feel free to fork, rice-out, pull-request, etc.
-
-Kudos to @jart for our lovely hiptext shenanigans ;)
+Kudos to jart for our hiptext shenangigans ;)
--- roll.sh
+++ roll.sh
@@ -4,24 +4,14 @@
 version='1.0'
 my_head=${BASH_SOURCE[0]}
 base_dir=$(dirname $my_head)
-script='astley.sh'
-
-NEVER="cp -f"
-# GONNA_GIVE="${base_dir}/${render}"
-# YOU_UP="$HOME/.${render}"  # Rendering destination.
-GONNA_TURN_AROUND="${base_dir}/${script}"
-AND_DESERT_YOU="$HOME/.${script}"  # Script destination.
 NEVER_GONNA_TELL_A_LIE="INT TERM EXIT STOP SUSPEND"  # For evil mode.
 AND_HURT_YOU="$HOME/.bashrc"
-
 # Terminal helpers (Assumes 256 colors).
 red='\x1b[38;5;9m'
 purp='\x1b[38;5;171m'
 yell='\x1b[38;5;216m'
 green='\x1b[38;5;10m'
-
 echo -e '\x1b[s'  # Save cursor
-
 usage () {
   echo -e "${green}Rick Astley performs ♪ Never Gonna Give You Up ♪ on STDOUT."
   echo -e "${yell}Usage: ./astley.sh [OPTIONS...]"
@@ -29,62 +19,32 @@ usage () {
   echo -e "${purp}Options:${yell}"
   echo -e "  help   - Show this message."
   echo -e "  inject - Append to ${purp}${USER}${yell}'s bashrc. (Recommended :D)"
-  echo "  evil   - No prompts. Replaces shell signals with Rick Roll lyrics."
-  echo "  stop   - Actually Gonna Give You Up. (Stop all roll'n processes) :("
   echo
 }
-
 prompt() {
   echo -ne "$yell > $1 $purp[y/n] $red"
   read input
   [[ "$input" == "y"* ]] && return 0
   return 1
 }
-
-clean() {
-  [[ -f "$YOU_UP.dl" ]] && rm "$YOU_UP.dl"
-  echo -e "${green}Goodbye. (I said it!)"
-  quit
-}
-
 quit() {
   echo -en "\x1b[?25h \x1b[0m"   # Reset cursor
   [[ -n $save_term ]] || echo -e "\x1b[2J \x1b[H <3"
   [[ -n $return_mode ]] && return 0 || exit 0
 }
-
-nanosec() {
-  date +%s%N
-}
-
-# Begin the things.
 for arg in "$@"; do
   if [[ "$arg" == "help"* || "$arg" == "-h"* ]]; then
-    argged=true
     usage && exit
-  elif [[ "$arg" == "inject"* ]]; then
-    argged=true
-    inject=true
-    save_term=true
   elif [[ "$arg" == "evil"* ]]; then
-    argged=true
     evil=true
-  elif [[ "$1" = "stop"* ]]; then
-    argged=true
-    save_term=true
-    clean
-  elif [[ "$arg" == "src"* ]]; then
-    return_mode=true
   else
     echo -e "${red}Unrecognized option: $arg"
     usage && exit
   fi
 done
-
 [[ $evil ]] && echo -en "${red}[Evil] "
 [[ $inject ]] && echo -en "${yell}[Injection]"
 echo
-
 if [[ $inject ]]; then
   # Just inject, no rendering fun this time. Do preserve the evil flag, though.
   [[ $evil ]] && earg="evil"
@@ -145,11 +105,8 @@ else
   trap "cleanup" INT
 fi
 trap "quit" EXIT
-
-# we know the game and we're gonna play it!
 echo -e "\x1b[2J"
-
-# Agnostic to curl or wget availability.
+# Bean streamin'- agnostic to curl or wget availability.
 obtainium() {
   if hash curl 2>/dev/null; then
     curl -s $1
@@ -157,26 +114,23 @@ obtainium() {
     wget -q -O - $1
   fi
 }
-
-# Bean streamin'
 bean="http://bean.vixentele.com/~keroserene"
 remote="$bean/astley80.full.bz2"
 
 if hash afplay 2>/dev/null; then
   # On Mac OS we pre-fetch compressed audio and use afplay.
-  #remote="$bean/astley80.mac.bz2"
   echo "downloading audio..."
   obtainium $bean/roll.gsm.wav >/tmp/roll.gsm.wav
   afplay /tmp/roll.gsm.wav &
   audpid=$!
-elif hash afplay 2>/dev/null; then
-  # On Linux if the aplay command is available, we stream raw sound.
+elif hash aplay 2>/dev/null; then
+  # On Linux if the aplay command is available, stream raw sound.
   obtainium $bean/roll.s16 | aplay -q -f S16_LE -r 8000 &
   audpid=$!
 fi
 
-# Print out frames, inserting pauses and skipping frames if necessary to stay
-# as close as possible to real time.
+# Sync FPS to reality as best as possible. Mac's freebsd version of date cannot
+# has nanoseconds so inject python. :/
 python <(cat <<EOF
 import sys
 import time
@@ -200,5 +154,4 @@ except KeyboardInterrupt:
   pass
 EOF
 ) < <(obtainium $remote | bunzip2 -q 2> /dev/null)
-
 echo -e '\x1b[u'  # Restore cursor position.
