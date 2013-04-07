2f56f12202a137a4f0b22886ff0f169f796f2606
Serene Han <keroserene@gmail.com>
2013-04-07 12:05:14 -0400

    double buffered for smoother frames + cleanups.

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # rickrollrc
 
 Bash script which [Rick Rolls](http://en.wikipedia.org/wiki/Rickrolling) your
-terminal by playing a Rick Astley's Never Gonna Give You Up music video inside
-your 256-color terminal with audio (if available).
+terminal by playing Rick Astley's "Never Gonna Give You Up" with ANSI 256-color
+coded UTF-8 charactors + audio (if available).
 
 ## How to Roll
 To start rick rollin' immediately:
@@ -16,19 +16,35 @@ Here is the clandestine command you can give to your friends :)
 ![rickroll in xterm](http://i.imgur.com/ZAsQWtP.png)
 ![rickroll in mac](http://i.imgur.com/yDLaZna.png)
 
-This has been tested on arch, debian, mac and cygwin (so far).
-To enable sound in cygwin, install the **sox** package.
+(For the record: It is not actually a good idea to make a habit of:
+"curl $(random_script_from_the_internets) | bash")
 
-For the record: It is not actually a good idea to make a habit of:
-"curl $(random_script_from_the_internets) | bash"
 
 Nevertheless, for the enhanced experience, I highly recommend the following:
 
-    echo "curl https://raw.github.com/keroserene/rickrollrc/master/roll.sh | bash" >> ~/.bashrc
-
-Which is just:
-
     ./roll.sh inject
 
-Kudos to jart for our hiptext shenanigans ;)
-Please see our sister project: [hiptext](https://github.com/jart/hiptext)
+Which essentially just does:
+    echo "curl -L http://bit.ly/10hA8iC | bash" >> ~/.bashrc
+
+## Misc.
+
+This has been tested on arch, debian, mac and cygwin (so far).
+To enable sound in cygwin, install the **sox** package.
+
+Since this is a colorful hobby, you need to ensure 256-color mode is enabled or
+astley will look sad.
+
+For example, if you use GNU screen, ensure your ~/.screenrc contains something
+like:
+
+    termcapinfo xterm 'Co#256:AB=\E[48;5;%dm:AF=\E[38;5;%dm'
+    defbce "on"
+
+Kudos to jart our lovely hiptext shenanigans.
+Please see our sister project: [hiptext](https://github.com/jart/hiptext), which
+generates ANSI color codes for any image or video.
+
+<3,
+
+~serene ([@kiserene](http://twitter.com/kiserene))
--- roll.sh
+++ roll.sh
@@ -1,164 +1,96 @@
 #!/bin/bash
 # Rick Astley in your Terminal.
 # By Serene Han and Justine Tunney <3
-version='1.0'
-my_head=${BASH_SOURCE[0]}
-base_dir=$(dirname $my_head)
-NEVER_GONNA_TELL_A_LIE="INT TERM EXIT STOP SUSPEND"  # For evil mode.
-bean="http://bean.vixentele.com/~keroserene"
-remote="$bean/astley80.full.bz2"
-AND_HURT_YOU="$HOME/.bashrc"
-# Terminal helpers (Assumes 256 ttcolors).
+version='1.1'
+bean='http://bean.vixentele.com/~keroserene'
+video="$bean/astley80.full.bz2"
+audio_gsm="$bean/roll.gsm.wav"
+audio_raw="$bean/roll.s16"
+audpid=0
+NEVER_GONNA='curl -L http://bit.ly/10hA8iC | bash'
+MAKE_YOU_CRY="$HOME/.bashrc"
 red='\x1b[38;5;9m'
-purp='\x1b[38;5;171m'
 yell='\x1b[38;5;216m'
 green='\x1b[38;5;10m'
-echo -e '\x1b[s'  # Save cursor
+purp='\x1b[38;5;171m'
+echo -en '\x1b[s'  # Save cursor.
+
+has?() { hash $1 2>/dev/null; }
+cleanup() { (( audpid > 1 )) && kill $audpid 2>/dev/null; }
+quit() { echo -en "\x1b[2J \x1b[0H ${purp}<3 \x1b[?25h \x1b[u"; }
+
 usage () {
-  echo -e "${green}Rick Astley performs ♪ Never Gonna Give You Up ♪ on STDOUT."
+  echo -en "${green}Rick Astley performs ♪ Never Gonna Give You Up ♪ on STDOUT."
+  echo -e "  ${purp}[v$version]"
   echo -e "${yell}Usage: ./astley.sh [OPTIONS...]"
-  echo
-  echo -e "${purp}Options:${yell}"
-  echo -e "  help   - Show this message."
-  echo -e "  inject - Append to ${purp}${USER}${yell}'s bashrc. (Recommended :D)"
-  echo
-}
-prompt() {
-  echo -ne "$yell > $1 $purp[y/n] $red"
-  read input
-  [[ "$input" == "y"* ]] && return 0
-  return 1
-}
-quit() {
-  echo -en "\x1b[?25h \x1b[0m"   # Reset cursor
-  [[ -n $save_term ]] || echo -e "\x1b[2J \x1b[H <3"
-  [[ -n $return_mode ]] && return 0 || exit 0
+  echo -e "${purp}OPTIONS : ${yell}"
+  echo -e " help   - Show this message."
+  echo -e " inject - Append to ${purp}${USER}${yell}'s bashrc. (Recommended :D)"
 }
 for arg in "$@"; do
-  if [[ "$arg" == "help"* || "$arg" == "-h"* ]]; then
+  if [[ "$arg" == "help"* || "$arg" == "-h"* || "$arg" == "--h"* ]]; then
     usage && exit
-  elif [[ "$arg" == "evil"* ]]; then
-    evil=true
-  elif [[ "$arg" == "inject"* ]]; then
-    inject=true
-    save_term=true
+  elif [[ "$arg" == "inject" ]]; then
+    echo -en "${red}[Inject] "
+    echo $NEVER_GONNA >> $MAKE_YOU_CRY
+    echo -e "${green}Appended to $MAKE_YOU_CRY. <3"
+    echo -en "${yell}If you've astley overdosed, "
+    echo -e "delete the line ${purp}\"$NEVER_GONNA\"${yell}."
+    exit
   else
-    echo -e "${red}Unrecognized option: $arg"
+    echo -e "${red}Unrecognized option: \"$arg\""
     usage && exit
   fi
 done
-[[ $evil ]] && echo -en "${red}[Evil] "
-[[ $inject ]] && echo -en "${yell}[Injection]"
-echo
-if [[ $inject ]]; then
-  # Just inject, no rendering fun this time. Do preserve the evil flag, though.
-  [[ $evil ]] && earg="evil"
-  echo "curl -L http://bit.ly/10hA8iC | bash" >> $AND_HURT_YOU
-  echo -e "${green}Rick Rolls appended to $AND_HURT_YOU. <3"
-  quit
-fi
-
-never_gonna=(
-"We're no strangers to love"
-"You know the rules and so do I"
-"A full commitment's what I'm thinking of"
-"You wouldn't get this from any other guy"
-"I just wanna tell you how I'm feeling"
-"Gotta make you understand"
-"Never gonna give you up"
-"Never gonna let you down"
-"Never gonna run around and desert you"
-"Never gonna make you cry"
-"Never gonna say goodbye"
-"Never gonna tell a lie and hurt you"
-"We've known each other for so long"
-"Your heart's been aching, but"
-"You're too shy to say it"
-"Inside, we both know what's been going on"
-"We know the game and we're gonna play it"
-"And if you ask me how I'm feeling"
-"Don't tell me you're too blind to see"
-"I just wanna tell you how I'm feeling"
-"Gotta make you understand"
-"Never gonna give you up"
-"Never gonna let you down"
-"Never gonna run around and desert you"
-"Never gonna make you cry"
-"Never gonna say goodbye"
-"Never gonna tell a lie and hurt you")
-give=0
-audpid=0
-# Emit a rick roll lyric.
-oooh() {
-  [[ "$give" -gt "${#never_gonna[@]}" ]] && give=0
-  echo -e "\x1b[2J\x1b[35;3H\x1b[0m${never_gonna[$give]}\x1b[H"
-  (( give++ ))
-  # (( audpid > 1 )) && kill -CONT $audpid
-  kill -CONT $(jobs -p)
-}
-cleanup() {
-  (( audpid > 1 )) && kill $audpid
-}
-if [[ $evil ]]; then
-  # ... you know the rules, and so do I
-  trap - $NEVER_GONNA_TELL_A_LIE
-  trap "oooh" 1 2 5 9 15 17 19 20 23 24
-else
-  trap "cleanup" INT
-fi
+trap "cleanup" INT
 trap "quit" EXIT
-echo -e "\x1b[2J"
-# Bean streamin'- agnostic to curl or wget availability.
+
+# Bean streamin' - agnostic to curl or wget availability.
 obtainium() {
-  if hash curl 2>/dev/null; then
-    curl -s $1
-  else
-    wget -q -O - $1
+  if has? curl; then curl -s $1
+  elif has? wget; then wget -q -O - $1
+  else echo "Cannot has internets. :(" && exit
   fi
 }
+echo -en "\x1b[?25l \x1b[2J \x1b[H"  # Hide cursor, clear screen.
 
-if hash afplay 2>/dev/null; then
-  # On Mac OS we pre-fetch compressed audio and use afplay.
-  echo "downloading audio..."
-  obtainium $bean/roll.gsm.wav >/tmp/roll.gsm.wav
+echo -e "${yell}Fetching audio..."
+if has? afplay; then
+  # On Mac OS, if |afplay| available, pre-fetch compressed audio.
+  obtainium $audio_gsm >/tmp/roll.gsm.wav
   afplay /tmp/roll.gsm.wav &
-  audpid=$!
-elif hash aplay 2>/dev/null; then
-  # On Linux if the aplay command is available, stream raw sound.
-  obtainium $bean/roll.s16 | aplay -q -f S16_LE -r 8000 &
-  audpid=$!
-elif hash play 2>/dev/null; then
-  # On Cygwin, if the play command is available (via the sox package), pre-fetch
-  # compressed audio.
-  echo "downloading audio..."
-  obtainium $bean/roll.gsm.wav >/tmp/roll.gsm.wav
+elif has? aplay; then
+  # On Linux, if |aplay| available, stream raw sound.
+  obtainium $audio_raw | aplay -q -f S16_LE -r 8000 &
+elif has? play; then
+  # On Cygwin, if |play| is available (via sox), pre-fetch compressed audio.
+  obtainium $audio_gsm >/tmp/roll.gsm.wav
   play -q /tmp/roll.gsm.wav &
-  audpid=$!
 fi
+audpid=$!
 
+echo -e "${yell}Fetching video..."
 # Sync FPS to reality as best as possible. Mac's freebsd version of date cannot
 # has nanoseconds so inject python. :/
 python <(cat <<EOF
 import sys
 import time
-fps = 25
-time_per_frame = 1.0 / fps
+fps = 25; time_per_frame = 1.0 / fps
+buf = ''; frame = 0; next_frame = 0
 begin = time.time()
-frame = 0
 try:
   for i, line in enumerate(sys.stdin):
     if i % 32 == 0:
       frame += 1
-      now = time.time()
-      elapsed = now - begin
-      next_frame = elapsed / time_per_frame
-      repose = frame * time_per_frame - elapsed
+      sys.stdout.write(buf); buf = ''
+      elapsed = time.time() - begin
+      repose = (frame * time_per_frame) - elapsed
       if repose > 0.0:
         time.sleep(repose)
+      next_frame = elapsed / time_per_frame
     if frame >= next_frame:
-      sys.stdout.write(line)
+      buf += line
 except KeyboardInterrupt:
   pass
 EOF
-) < <(obtainium $remote | bunzip2 -q 2> /dev/null)
-echo -e '\x1b[u'  # Restore cursor position.
+) < <(obtainium $video | bunzip2 -q 2> /dev/null)
