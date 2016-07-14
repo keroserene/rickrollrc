b4c5d4cf6b251045efe49dead823ec0397ca6de5
Phitherek_ <phitherek@gmail.com>
2016-07-14 12:58:00 +0200

    Use paplay only if aplay not available

--- roll.sh
+++ roll.sh
@@ -61,11 +61,11 @@ if has? afplay; then
   # On Mac OS, if |afplay| available, pre-fetch compressed audio.
   [ -f /tmp/roll.s16 ] || obtainium $audio_raw >/tmp/roll.s16
   afplay /tmp/roll.s16 &
-elif has? paplay; then
-  obtainium $audio_raw | paplay --format=s16le --rate=8000 &
 elif has? aplay; then
   # On Linux, if |aplay| available, stream raw sound.
   obtainium $audio_raw | aplay -Dplug:default -q -f S16_LE -r 8000 &
+elif has? paplay; then
+  obtainium $audio_raw | paplay --format=s16le --rate=8000 &
 elif has? play; then
   # On Cygwin, if |play| is available (via sox), pre-fetch compressed audio.
   obtainium $audio_gsm >/tmp/roll.gsm.wav
